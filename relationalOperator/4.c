2d877060eb3790eed380d21171761d7e3ebaebfd
unknown <objadhav28@gmail.com>
2024-04-07 14:06:08 +0530

    using '==' this equality operator

---
+++ relationalOperator/4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a = 0; 
+    int b = 0;
+
+    printf("Please enter a First number:\n");
+    scanf("%d", &a);
+
+    printf("Please enter a Second number:\n");
+    scanf("%d", &b);
+
+    if(a == b) 
+    {
+        printf("Value of 'a' is exact equals to 'b'.\n");
+    } else 
+    {
+        printf("Value of 'a' is not exact equals to 'b'.\n");
+    }
+
+    return 0;
+}
