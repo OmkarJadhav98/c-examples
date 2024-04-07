12de61b3da9afbabb4548e0d4e9f615b73a4ee1a
unknown <objadhav28@gmail.com>
2024-04-07 14:04:39 +0530

    using '<' this equality operator

---
+++ relationalOperator/1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a = 0; 
+    int b = 0;
+
+    printf("Please enter a First number:\n");
+    scanf("%d", &a);
+
+    printf("Please enter a Second number:\n");
+    scanf("%d", &b);
+
+    if(a < b) 
+    {
+        printf("Value of 'a' is Smaller than 'b'.\n");
+    } else 
+    {
+        printf("Value of 'a' is not Smaller than 'b'.\n");
+    }
+
+    return 0;
+}
