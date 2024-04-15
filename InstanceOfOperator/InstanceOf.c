9694b4bcf882723e7088872024d891862bd1197a
unknown <objadhav28@gmail.com>
2024-04-15 14:22:18 +0530

    Using InstanceOf Operator

---
+++ InstanceOfOperator/InstanceOf.c
@@ -0,0 +1,21 @@
+/*=====================================================================================================================*/
+// In C, there is no direct equivalent to Java instanceof operator. Instead of instanceof, I write this code in C.
+/*=====================================================================================================================*/
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() 
+{
+    char city[] = "Pune";
+    int length = 20;
+    double temp = 35.5;
+
+    printf("This type of city is String\n");
+
+    printf("This type of length is Integer\n");
+
+    printf("This type of temp is Double\n");
+
+    return 0;
+}
