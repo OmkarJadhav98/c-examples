b5fec35e0120fc32f996cc65d6130d1f36ffa9e0
unknown <objadhav28@gmail.com>
2024-04-23 12:28:13 +0530

    Using For loop

---
+++ ArmstrongNumber/2.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int Armstrong(int No) 
+{
+    int originalNumber = No;
+    int remainder = 0;
+    int result = 0;
+    int iNo = 0;
+
+    // Count the number of digits in No
+    for (;originalNumber != 0;originalNumber /= 10) 
+    {
+        
+        ++iNo;
+    }
+
+    originalNumber = No;
+
+    // Calculate the sum of the nth powers of its digits
+    for (;originalNumber != 0;originalNumber /= 10) 
+    {
+        remainder = originalNumber % 10;
+        int power = 1;
+        int i = 0;
+        for (;i < iNo;i++) 
+        {
+            power *= remainder;
+            
+        }
+
+        result += power;
+        
+    }
+
+    // Check if No is an Armstrong number
+    return No == result;
+}
+
+int main() 
+{
+    printf("Please enter a number to check if it's an Armstrong number: \n");
+
+    int No;
+    scanf("%d", &No);
+
+    if (Armstrong(No)) 
+    {
+        printf("%d is an Armstrong number", No);
+    } else 
+    {
+        printf("%d is not an Armstrong number", No);
+    }
+
+    return 0;
+}
