7d6433abd5cb027ef264149cfa33e63f1e6fedde
unknown <objadhav28@gmail.com>
2024-04-30 21:22:52 +0530

    using Do-While loop

---
+++ PrimeNumber/3.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdbool.h> // Include the header for bool data type
+
+bool isPrime(int num)
+{
+    if (num <= 1)
+    {
+        return false;
+    }
+    if (num == 2)
+    {
+        return true;
+    }
+
+    int i = 2;
+    do
+    {
+        if (num % i == 0)
+        {
+            return false;
+        }
+        ++i;
+    }while (i * i <= num); // Using i * i for efficiency instead of calculating sqrt(num)
+
+    return true;
+}
+
+int main()
+{
+    int number = 0;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &number);
+
+    if (isPrime(number))
+    {
+        printf("%d is a prime number.\n", number);
+    }
+    else
+    {
+        printf("%d is not a prime number.\n", number);
+    }
+
+    return 0;
+}
